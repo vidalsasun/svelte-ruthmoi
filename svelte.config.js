b0d61fe059cc44290a4a478e0984928ca1777799
Carlos Javier Moreira <cmoreira@ceieu.com>
2024-08-11 20:24:55 +0200

    try heroku 1

--- svelte.config.js
+++ svelte.config.js
@@ -2,6 +2,7 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import adapter from "@sveltejs/adapter-static";
 //import adapter from '@sveltejs/adapter-vercel';
+import node from "@sveltejs/adapter-node";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -13,12 +14,14 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter({
+		/*adapter: adapter({
             // opciones del adaptador
             pages: 'build',
             assets: 'build',
             fallback: '404.html'
-        }),
+        }),*/
+		target: "#svelte",
+		adapter: node({ env: { port: process|.env.PORT } }),
 		alias: {
 			"@/*": "./src/lib/*",
 		  },
