bdc2d799774d5d413cfaeb4fb6bba381be233554
Carlos Javier Moreira <cmoreira@ceieu.com>
2024-08-11 20:26:28 +0200

    try heroku 2

--- svelte.config.js
+++ svelte.config.js
@@ -14,14 +14,12 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		/*adapter: adapter({
+		adapter: adapter({
             // opciones del adaptador
             pages: 'build',
             assets: 'build',
             fallback: '404.html'
-        }),*/
-		target: "#svelte",
-		adapter: node({ env: { port: process|.env.PORT } }),
+        }),
 		alias: {
 			"@/*": "./src/lib/*",
 		  },
