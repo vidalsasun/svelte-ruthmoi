f6bf9e16cde54b244934683573bd2cccdd9484aa
Carlos Javier Moreira <cmoreira@ceieu.com>
2024-09-02 15:58:55 +0200

    vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 //import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import adapter from "@sveltejs/adapter-static";
-//import adapter from '@sveltejs/adapter-vercel';
+//import adapter from "@sveltejs/adapter-static";
+import adapter from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -13,12 +13,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter({
-            // opciones del adaptador
-            pages: 'build',
-            assets: 'build',
-            fallback: '404.html'
-        }),
+		adapter: adapter(),
 		alias: {
 			"@/*": "./src/lib/*",
 		  },
